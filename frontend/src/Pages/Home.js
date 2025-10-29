de658a6def3c7a3e5b9cc61c3b65628f853136fe
Vaibhav3715 <vaibhavsinghal946@gmail.com>
2025-10-29 20:17:31 +0530

    Fix: removed unused imports

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,28 +2,21 @@ import React from 'react'
 
 import About from '../components/About'
 import Footer from '../components/Footer'
-import ImageSlider from '../components/ImageSlider'
-import News from '../components/News'
-import PhotoGallery from '../components/PhotoGallery'
 import PopularBooks from '../components/PopularBooks'
 import RecentAddedBooks from '../components/RecentAddedBooks'
-import ReservedBooks from '../components/ReservedBooks'
-import Stats from '../components/Stats'
 import WelcomeBox from '../components/WelcomeBox'
 
 function Home() {
     return (
         <div id='home'>
-            <WelcomeBox/>
-            
-            <RecentAddedBooks/>
-            <PopularBooks/>
-            
-        
-            <About/>
-            <Footer/>
+            <WelcomeBox />
+            <RecentAddedBooks />
+            <PopularBooks />
+            <About />
+            <Footer />
         </div>
     )
 }
 
 export default Home
+
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import './Footer.css'
-
-import TwitterIcon from '@material-ui/icons/Twitter';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import TelegramIcon from '@material-ui/icons/Telegram';
-import InstagramIcon from '@material-ui/icons/Instagram';
+import React from 'react';
+import './Footer.css';
 
 function Footer() {
-    return (
-        <div className='footer'>
-            <div>
-                <div className='footer-data'>
-                    <div className="contact-details">
-                        <h1>Contact Us</h1>
-                        <p><b>Email:</b>prachisinghal@gmail.com</p>
-                    </div>
-                    <div className='librarian-details'>
-                        <h1>Librarian</h1>
-                        <p>Prachi Singhal</p>
-                        <p>Btech Graduate</p>
-                        <p>Contact: +91 943569321</p>
-                    </div>
-                </div>
-            </div>
-            <div className='copyright-details'>
-                <p className='footer-copyright'>&#169; 2025 copyright all right reserved<br /><span>Designed with ❤️ by Prachi Singhal</span></p>
-            </div>
+  return (
+    <div className='footer'>
+      <div>
+        <div className='footer-data'>
+          <div className="contact-details">
+            <h1>Contact Us</h1>
+            <p><b>Email:</b> prachisinghal@gmail.com</p>
+          </div>
+          <div className='librarian-details'>
+            <h1>Librarian</h1>
+            <p>Prachi Singhal</p>
+            <p>BTech Graduate</p>
+            <p>Contact: +91 943569321</p>
+          </div>
         </div>
-    )
+      </div>
+      <div className='copyright-details'>
+        <p className='footer-copyright'>
+          &#169; 2025 All rights reserved<br />
+          <span>Designed with ❤️ by Prachi Singhal</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
